d42fd1fe2823ab0b424280bc1849c87c90b76e09
eden6644 <edenraiven@gmail.com>
2022-09-21 00:43:47 +0200

    spoiler list

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 1. dès qu'on clique sur le boutton, on ajoute la classe visible à l'élement suivant
 
 
-*/
+
 
 var btS = document.querySelector('.spoiler button')
 
@@ -11,3 +11,15 @@ btS.addEventListener('click', function(){
   this.nextElementSibling.classList.add('visible')
   this.parentNode.removeChild(this)
 })
+*/
+
+var spoilers = document.querySelectorAll('.spoiler');
+
+let createSpoilerbtn = function(element){
+  let btnS = document.createElement('button')
+  btnS.textContent = "Attention Spoiler"
+  element.appendChild(btnS)
+}
+for (let i = 0; i < spoilers.length; i++) {
+  createSpoilerbtn(spoilers[i])
+}
