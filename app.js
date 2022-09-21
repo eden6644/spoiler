42516590960933d1f4799176e86299400c20e149
eden6644 <edenraiven@gmail.com>
2022-09-21 19:15:29 +0200

    okay for this

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+
+//1. dès qu'on clique sur le boutton, on ajoute la classe visible à l'élement suivant
+
+
 /*
-1. dès qu'on clique sur le boutton, on ajoute la classe visible à l'élement suivant
-
-
-
 
 var btS = document.querySelector('.spoiler button')
 
@@ -13,17 +13,41 @@ btS.addEventListener('click', function(){
 })
 */
 
-var spoilers = document.querySelectorAll('.spoiler');
 
+
+//ETAPE 1: recupération des element ayant la classe spoiler et preparation d'une foinction pour 
+// creer des boutton  et une span de clase spoiler-content pour chacun de ses elements
+var spoilers = document.querySelectorAll('.spoiler');
 let createSpoilerbtn = function(element){
+  
+  //creation button
   let btnS = document.createElement('button')
   btnS.textContent = "Attention Spoiler"
-  element.appendChild(btnS)
+  
+//-------------------------------------------------------------------------fin etape 1
 
+
+
+  //creation span
   let spanSpoilerContent = document.createElement('span')
   spanSpoilerContent.className = "spoiler-content"
-  spanSpoilerContent.textContent = spoilers.textContent
+  spanSpoilerContent.innerHTML = element.innerHTML
+
+
+  //ajout du buton et du span au Dom
+  // d'ab on nettoie l'éelement ancien
+  element.innerHTML  = ""
+  element.appendChild(btnS)
+  element.appendChild(spanSpoilerContent)
+
+  // event sur les btn
+  btnS.addEventListener('click', function(){
+    btnS.parentNode.removeChild(btnS)
+    spanSpoilerContent.classList.add('visible')
+  })
 }
+
+// étape 2 création des boutton pour chaques span spoiler
 for (let i = 0; i < spoilers.length; i++) {
   createSpoilerbtn(spoilers[i])
 }
